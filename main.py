9e19d744d5ec28882b53e2bf62e6daa1d7d10dab
Abdelaziz Wael <abdelaziz.w.f@gmail.com>
2022-11-04 20:12:12 +0200

    used pandas, major refactoring

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
 # main.py
 
-from preprocessing import preprocess_data
-from model import Model
-import logging
-from plots import plot_features
 from itertools import combinations
-from util import *
+import logging
 from pathlib import Path
+from random import shuffle
 
-def read_data(path):
-    data = []
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-    with open(path, 'r') as f:
-        for line in f:
-            data.append(line.strip().split(','))
+from model import Model
 
-    return data
 
-def pre_plots():
-    data = read_data('penguins.csv')
-    path = Path('.') / 'plots'
-    preprocess_data(data)
-    for f1, f2 in combinations(data[0][1:], 2):
-        selected_data = extract_features(data, f1, f2)
-        plot_features(selected_data, path)
+PLOT_PATH = Path('./plots/')
 
-def main():
-    logging.info('Starting...')
 
-    data = read_data('penguins.csv')
-    preprocess_data(data)
+# Draw and save the 10 feature plots
+def pre_plots(data):
+    features = list(data.columns)
+    features.remove('species')
+    for f1, f2 in combinations(features, 2):
+        plt.xlabel(f1)
+        plt.ylabel(f2)
 
-    logging.info('Preprocessing Done.')
+        for name, group in data.groupby('species'):
+            plt.scatter(group[f1], group[f2], label=name)
 
-    feature1 = 'bill_depth_mm'
-    feature2 = 'bill_length_mm'
+        plt.legend()
 
-    a = get_class_slice(C1)
-    b = get_class_slice(C2)
-    c = get_class_slice(C3)
+        path = PLOT_PATH / f"{f1}-{f2}"
+        plt.savefig(path, dpi=150)
+        plt.clf()
 
-    selected_data = extract_features(data, feature1, feature2)
 
-    logging.info(f"Extracted features: {feature1} and {feature2}")
+# Black magick
+def partition_data(data):
+    training, testing, = [], []
 
-    model = Model(selected_data[a], selected_data[b])
+    d = data.groupby('species')
+    for _, group in d:
+        training += list(group.values)[:30]
+        testing += list(group.values)[30:]
+
+    shuffle(training)
+    shuffle(testing)
+
+    return [d[:2] for d in training], [d[2] for d in training], [d[:2] for d in testing], [d[2] for d in testing]
+
+
+def preprocess(data):
+    data.fillna('pad', inplace=True) # Replace Na values with the last valid value of their column
+    data['gender'] = np.where(data['gender'] == 'male', 1, 0) # Convert `male` to 1, `female` to 0
+    data['flipper_length_mm'] = data['flipper_length_mm'] / 10 # Convert `flipper_length_mm` to Cm
+    data['body_mass_g'] = data['body_mass_g'] / 1000 # Convert `body_mass_g` to Kg
+
+def run(data):
+    features = ['bill_depth_mm', 'bill_length_mm']
+    species = ['Gentoo', 'Chinstrap']
+
+    filt = data['species'].isin(species)
+    sel_data = data.loc[filt, features + ['species']]
+
+    x_train, y_train, x_test, y_test = partition_data(sel_data)
+    logging.info(f"Extracted features: {features} for species {species}")
+
+    model = Model(x_train, y_train, x_test, y_test, species)
     logging.info('Initialized the model')
 
     model.train()
@@ -61,4 +81,13 @@ def main():
 if __name__ == '__main__':
     logging.basicConfig(filename='run.log', filemode='w', level=logging.DEBUG)
 
-    main()
+    logging.info('Starting...')
+
+    plt.style.use('ggplot')
+
+    data = pd.read_csv('penguins.csv')
+    preprocess(data)
+
+    logging.info('Preprocessing Done.')
+
+    run(data)
--- model.py
+++ model.py
@@ -3,78 +3,52 @@ import random
 
 class Model:
 
-    def __init__(self, data1, data2, eta=0.4, epochs=750, bias_flag=1):
-        """
-        data = [
-            ['class', 'f1', 'f2'],
-            ['class', 'f1', 'f2'],
-            ...
-        ]
-        """
-        # Partitioning data
-        self.c1_train, self.c1_test = self.partition(data1)
-        self.c2_train, self.c2_test = self.partition(data2)
+    def __init__(self, x_train, y_train, x_test, y_test, labels, eta=0.4, epochs=750, bias_flag=True):
+        self.x_train, self.y_train = x_train, y_train
+        self.x_test, self.y_test = x_test, y_test
         self.eta = eta # Learning rate
         self.epochs = epochs
-        self.bias_flag = bias_flag
+        self.x0 = 1 if bias_flag else 0
 
-        self.label_map = {data1[0][0]: -1, data2[0][0]: 1} # Representing classes as -1 and 1
+        self.label_map = {labels[0]: -1, labels[1]: 1} # Representing classes as -1 and 1
         self.weights = [random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)] # Initializing weights
 
         logging.debug(f'Model initialized with: eta={eta}, epochs={epochs}, bias_flag={bias_flag}')
         logging.debug(f"Label Map: {self.label_map}")
         logging.debug(f'Starting weights: {self.weights}')
 
-    def partition(self, data):
-        return data[:30], data[30:]
-
     def train(self):
-        # Combine training data and shuffle it
-        training_data = self.c1_train + self.c2_train
-        random.shuffle(training_data)
-        logging.debug(f"Training Data: {training_data}")
-
-        x0 = 1 if self.bias_flag else 0 # Set the bias input according to the bias flag
-
         for i in range(self.epochs):
-            self._train(training_data, x0)
+            for [x1, x2], label in zip(self.x_train, self.y_train):
+                t = self.label_map[label] # Set the target to -1 or 1 according to the class
+
+                # Calculate the net value (W^T * X)
+                net = (self.x0 * self.weights[0]) + (x1 * self.weights[1]) + (x2 * self.weights[2])
+
+                # Activation function
+                if net < 0:
+                    y = -1
+                else:
+                    y = 1
+
+                # Calculate error
+                e = t - y
+
+                # Update the weights
+                self.weights[0] += self.eta * e * self.x0
+                self.weights[1] += self.eta * e * x1
+                self.weights[2] += self.eta * e * x2
+
             logging.debug(f'Weights after epoch {i + 1}: {self.weights}')
 
-    def _train(self, data, x0):
-        for label, x1, x2 in data:
-            t = self.label_map[label] # Set the target to -1 or 1 according to the class
-
-            # Calculate the net value (W^T * X)
-            net = (x0 * self.weights[0]) + (x1 * self.weights[1]) + (x2 * self.weights[2])
-
-            # Activation function
-            if net < 0:
-                y = -1
-            else:
-                y = 1
-
-            # Calculate error
-            e = t - y
-
-            # Update the weights
-            self.weights[0] += self.eta * e * x0
-            self.weights[1] += self.eta * e * x1
-            self.weights[2] += self.eta * e * x2
-
     def test(self):
-        # Combine the test data and shuffle it
-        test_data = self.c1_test + self.c2_test
-        random.shuffle(test_data)
-        logging.debug(f"Testing Data: {test_data}")
-        
-        x0 = 1 if self.bias_flag else 0 # Set the bias input according to the bias flag
         correct = 0 # A counter for the correct prediction made by the model
 
-        for label, x1, x2 in test_data:
+        for [x1, x2], label in zip(self.x_test, self.y_test):
             t = self.label_map[label] # Set the target to -1 or 1 according to the class
 
             # Calculate the net value (W^T * X)
-            net = (x0 * self.weights[0]) + (x1 * self.weights[1]) + (x2 * self.weights[2])
+            net = (self.x0 * self.weights[0]) + (x1 * self.weights[1]) + (x2 * self.weights[2])
 
             # Activation function
             if net < 0:
@@ -87,4 +61,4 @@ class Model:
                 correct += 1
 
         # Return the accuracy as a percentage
-        return (correct / len(test_data)) * 100
+        return (correct / len(self.y_test)) * 100
